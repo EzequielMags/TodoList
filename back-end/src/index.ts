f7e88a76dfc1243df306e6b4c7f7b975efe61437
Ezequiel Magoga <ezequielmagoga2005@gmail.com>
2026-09-02 11:15:45 -0300

    feat: adding others

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,6 +1,16 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
+import temaRoutes from './modules/tema/tema.routes.js'
+import todoRoutes from './modules/todo/todo.routes.js'
 
 const server = fastify()
+
+server.register(cors, {
+    origin: true
+})
+
+server.register(temaRoutes)
+server.register(todoRoutes)
 
 server.listen({ port: 3000 }, (err, address) => {
     if (err) {
